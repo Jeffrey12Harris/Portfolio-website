de61de6020c0c6647d9e8467c7ca40726c5638c8
Jeffrey Harris <jeffrey2006harris@gmail.com>
2024-09-16 14:43:39 -0700

    commit

---
+++ Spooky-site/main.js
@@ -0,0 +1,7 @@
+import './CSS/style.css';
+import './CSS/header.css';
+import './CSS/About.css';
+import './CSS/About2.css';
+import './CSS/My-projects.css';
+import './CSS/footer.css';
+import './CSS/utils.css';
